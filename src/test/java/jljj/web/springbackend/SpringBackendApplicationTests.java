4ab9fe6b90c33ee392f6097c454d2b75877e800b
Jonathan Lim <babyjonathanlim@gmail.com>
2025-03-03 23:31:19 +0800

    [Add] Spring

---
+++ src/test/java/jljj/web/springbackend/SpringBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package jljj.web.springbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
